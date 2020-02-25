aae368e16e8c0b6f01950771493c1a37a3463faf
Nathan St.Amand <nathansaint810@gmail.com>
2020-02-25 19:55:48 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
-# Add your code here
+class Dog 
+  @@all = []
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name 
+    save 
+    
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.clear_all
+    @@all = []
+  end
+  
+  def self.print_all 
+    new_string = []
+    @@all.each do |i|
+      new_string << "#{i.name + "\n"}"
+    end 
+    puts new_string.join
+  end
+  
+  def save 
+    @@all << self 
+  end 
+  
+end 
